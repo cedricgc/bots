573563b2f7bd4f7e8873ae0ceac744dfd5a2b2a5
Cedric Charly <cedric.charly@gmail.com>
2016-04-15 18:18:20 +0000

    Add ex_json_schema and httpoison deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bots.Mixfile do
   def application do
     [mod: {Bots, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,13 +30,17 @@ defmodule Bots.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
+    [
+     {:phoenix, "~> 1.1.4"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_ecto, "~> 2.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ex_json_schema, "~> 0.3.1"},
+     {:httpoison, "~> 0.8.3"}
+   ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
