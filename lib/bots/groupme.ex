7ee0d1c94c1d32740e570eeac9cf0e93480af855
Cedric Charly <cedric.charly@gmail.com>
2016-04-17 17:48:46 +0000

    send_bot_message now checks HTTP response

--- lib/bots/groupme.ex
+++ lib/bots/groupme.ex
@@ -1,9 +1,15 @@
 defmodule Bots.GroupMe do
+  require Logger
+
   @base_url "https://api.groupme.com/v3"
   @headers [{"Content-Type", "application/json"}]
 
   def send_bot_message(bot_id, message) do
     body = %{"bot_id" => bot_id, "text" => message} |> Poison.encode!
-    HTTPoison.post(@base_url <> "/bots/post", body, @headers)
+    case HTTPoison.post(@base_url <> "/bots/post", body, @headers) do
+      {:ok, %HTTPoison.Response{status_code: 202}} -> Logger.info("Posted bot message to GroupMe: #{message}")
+      {:ok, %HTTPoison.Response{status_code: 400}} -> Logger.error("GroupMe did not accept message")
+      {:error, err} -> Logger.error("Failed to post message to GroupMe")
+    end
   end
 end
