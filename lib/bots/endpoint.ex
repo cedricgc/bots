e91acf57a0802c37a104e6b7416502db0065744f
Cedric Charly <cedric.charly@gmail.com>
2016-04-28 06:00:27 +0000

    Make Plug.Static use /static as static content route

--- lib/bots/endpoint.ex
+++ lib/bots/endpoint.ex
@@ -8,7 +8,7 @@ defmodule Bots.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :bots, gzip: false,
+    at: "/static", from: :bots, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
