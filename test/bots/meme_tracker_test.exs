bce9050740998995ec1eceb58b8b3ef9c8b941ac
Cedric Charly <cedric.charly@gmail.com>
2016-05-05 03:00:57 +0000

    Fix spacing issues

--- test/bots/meme_tracker_test.exs
+++ test/bots/meme_tracker_test.exs
@@ -1,23 +1,23 @@
 defmodule Bots.MemeTrackerTest do
-   use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-   setup context do
-     {:ok, pid} = Bots.MemeTracker.start_link(context.test)
-     {:ok, pid: pid}
-   end
+  setup context do
+    {:ok, pid} = Bots.MemeTracker.start_link(context.test)
+    {:ok, pid: pid}
+  end
 
-   test "stores name for user", %{pid: pid} do
-     assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
+  test "stores name for user", %{pid: pid} do
+    assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
 
-     Bots.MemeTracker.set_meme(pid, "1234567", "takeonme.gif", :add)
-     assert Bots.MemeTracker.get_meme(pid, "1234567") == %{:name => "takeonme.gif", :action => :add}
-   end
+    Bots.MemeTracker.set_meme(pid, "1234567", "takeonme.gif", :add)
+    assert Bots.MemeTracker.get_meme(pid, "1234567") == %{:name => "takeonme.gif", :action => :add}
+  end
 
-   test "delete meme metadata for user", %{pid: pid} do
-     assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
+  test "delete meme metadata for user", %{pid: pid} do
+    assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
 
-     Bots.MemeTracker.set_meme(pid, "1234567", "takeonme.gif", :add)
-     Bots.MemeTracker.get_and_delete_meme(pid, "1234567")
-     assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
-   end
+    Bots.MemeTracker.set_meme(pid, "1234567", "takeonme.gif", :add)
+    Bots.MemeTracker.get_and_delete_meme(pid, "1234567")
+    assert Bots.MemeTracker.get_meme(pid, "1234567") == nil
+  end
  end
