370bb1ebe7c69d720c07df8c62108dd9e414b7b7
Cedric Charly <cedric.charly@gmail.com>
2017-01-26 19:40:14 -0600

    Bump version to 1.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bots.Mixfile do
 
   def project do
     [app: :bots,
-     version: "1.0.0",
+     version: "1.2.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
